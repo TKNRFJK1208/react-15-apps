c0802a59cd1bef70b8d96499a0356f36dc062fe9
takanori <takanori@goopter.com>
2022-01-10 18:03:54 -0800

    added sidebar-modal project files

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,45 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+ // Sidebar functions
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+  // Modal functions
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  return (
+    <AppContext.Provider 
+      value={{
+        isModalOpen, 
+        isSidebarOpen, 
+        openModal, 
+        openSidebar,
+        closeModal,
+        closeSidebar
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+// custom hook
+// Cutom hook should starts with "use"
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+}
+
+export { AppContext, AppProvider }
